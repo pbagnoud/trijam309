f8e493154b50e1b884bb2b665d9fa01a91a846a7
VB <119983496+vibonnier@users.noreply.github.com>
2025-02-15 15:27:49 +0100

    DICE CHANGE FACES AND REFRESH

--- tri-jam-309/script/dice.gd
+++ tri-jam-309/script/dice.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-var id: int
+@export var id: int
 var faces: Array[int] = [1,2,3,4,5,6] # the faces of the dice
 var current_faces: Array[int] = [1,2,3,4,5,6] # the faces of the dices at the current time
 var rng = RandomNumberGenerator.new() # Create a random number generator
 signal result(value1, value2) # value1 = id, value2 = result of the function reroll
 
-	
+
+func _ready():
+	visible = false  # Hide the button initially
+
+
 func reroll(current_faces):
 	var random_index = rng.randi_range(0, current_faces.size() - 1) # Generate a random index within the array bounds
 	var selected_face = current_faces[random_index] # Retrieve the face at the random index
@@ -16,3 +20,12 @@ func reroll(current_faces):
 	result.emit(id, selected_face)
 	
 	current_faces[random_index] = 0
+
+
+func refresh(current_faces):
+	current_faces = faces # re-initialize the faces of the dices
+
+
+func start_dice(valeurs):
+	faces = valeurs
+	visible = true
