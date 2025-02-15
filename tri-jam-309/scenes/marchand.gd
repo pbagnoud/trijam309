b8bd075d74a5b3ecbc11f86038e8bd84f36135b4
SolennEstier <solenn.estier@gmail.com>
2025-02-15 15:12:16 +0100

    retiré window

--- tri-jam-309/scenes/marchand.gd
+++ tri-jam-309/scenes/marchand.gd
@@ -1,6 +1,8 @@
-extends Window
+extends Node2D
 
-signal achat 
+signal achat
+
+@onready var options_des = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_close_requested() -> void:
-	visible = false
 
 
 func _on_option_1_pressed() -> void:
