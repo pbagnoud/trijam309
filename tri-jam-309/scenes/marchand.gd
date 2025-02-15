0ad0cd55a6c8ae9c7af9ff7402e8728d2870893e
SolennEstier <solenn.estier@gmail.com>
2025-02-15 15:44:24 +0100

    Ajouté choix des options

--- tri-jam-309/scenes/marchand.gd
+++ tri-jam-309/scenes/marchand.gd
@@ -2,11 +2,23 @@ extends Node2D
 
 signal achat
 
-@onready var options_des = []
+var options_des = [[1,2,3,4,5,6], [1,1,1,2,2,2]]
 
+var option1 = [1,2,3,4,5,6]
+var option2 = [1,2,3,4,5,6]
+var option3 = [1,2,3,4,5,6]
+
+
+var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var rand_option1 = rng.randi_range(0, len(options_des)-1)
+	var rand_option2 = rng.randi_range(0, len(options_des)-1)
+	var rand_option3 = rng.randi_range(0, len(options_des)-1)
+	option1 = options_des[rand_option1]
+	option2 = options_des[rand_option2]
+	option3 = options_des[rand_option3]
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,12 +27,18 @@ func _process(delta: float) -> void:
 
 
 func _on_option_1_pressed() -> void:
-	print("option1")
+	print("acheté option1 :", option1 )
+	# Emettre le signal "achat" en transmettant aussi le contenu du dé acheté
+	visible = false
 
 
 func _on_option_2_pressed() -> void:
-	print("option2")
+	print("acheté option2 :", option2 )
+	# Emettre le signal "achat" en transmettant aussi le contenu du dé acheté
+	visible = false
 
 
 func _on_option_3_pressed() -> void:
-	print("option3")
+	print("acheté option3 :", option3)
+	# Emettre le signal "achat" en transmettant aussi le contenu du dé acheté
+	visible = false
