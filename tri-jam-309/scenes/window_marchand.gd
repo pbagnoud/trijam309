cea3e4c2948bcf3712aca94c42586203219abb30
SolennEstier <solenn.estier@gmail.com>
2025-02-15 15:05:33 +0100

    Commencé marchand

---
+++ tri-jam-309/scenes/window_marchand.gd
@@ -0,0 +1,28 @@
+extends Window
+
+signal achat 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_close_requested() -> void:
+	visible = false
+
+
+func _on_option_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_3_pressed() -> void:
+	pass # Replace with function body.
