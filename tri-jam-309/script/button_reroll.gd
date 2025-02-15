fabdab17769d76066888a4e9206a9603e47faa3f
VB <119983496+vibonnier@users.noreply.github.com>
2025-02-15 16:16:53 +0100

    reroll dice button visible with activation

--- tri-jam-309/script/button_reroll.gd
+++ tri-jam-309/script/button_reroll.gd
@@ -1,6 +1,7 @@
 extends Button
 
 @onready var dice: Node2D = $".."
+@export var activated: bool = true # if desactivated then can't show the button
 
 func _ready():
 	visible = false  # Hide the button initially
@@ -8,7 +9,7 @@ func _ready():
 
 func _input(event):
 	
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and activated:
 		# Get the position of the mouse relative to the die asset
 		var mouse_pos = event.position
 		# Get the global position and size of the dice
@@ -18,7 +19,8 @@ func _input(event):
 		# Create a rectangle representing the dice bounds
 		var dice_rect = Rect2(dice_global_position - (dice_size / 2), dice_size)  # Centering around global position
 		
-		if dice_rect.has_point(mouse_pos):
+		# Check if 'activated' is not zero before showing the button
+		if activated == true and dice_rect.has_point(mouse_pos):
 			visible = true
 			position = mouse_pos + Vector2(0, 20)
 		else:
@@ -26,4 +28,4 @@ func _input(event):
 		
 	elif event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT and visible:
-			dice.reroll(dice.current_faces)  # Call your function when clicking on the button
+			dice.reroll()  # Call your function when clicking on the button
